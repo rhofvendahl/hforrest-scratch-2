bfa8ec9f8baf7d679398228d98a8c7c38d751096
LeePedersen <leepedersenr@gmail.com>
2023-02-23 21:55:06 -0700

    fix routing problems

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Video from './components/video';
 import Contact from './components/contact';
 import ViewImage from './components/viewImage';
 import Error404 from './components/Error404';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 class App extends React.Component {
 
@@ -40,14 +40,14 @@ class App extends React.Component {
     return (
       <div className="background">
         {this.state.useMenu ? <Menu /> : <Header />}
-        <Routes>
-          <Route exact path='/' element={Sculpture} />
-          <Route path='/about' element={About} />
-          <Route path='/video' element={Video} />
-          <Route path='/contact' element={Contact} />
-          <Route path='/viewImage' element={ViewImage} />
-          <Route element={Error404} />
-        </Routes>
+          <Routes>
+            <Route exact path='/' element={<Sculpture/>} />
+            <Route path='/about' element={<About/>} />
+            <Route path='/video' element={<Video/>} />
+            <Route path='/contact' element={<Contact/>} />
+            <Route path='/viewImage' element={<ViewImage/>} />
+            <Route element={Error404} />
+          </Routes>
         <Footer />
       </div>
     );

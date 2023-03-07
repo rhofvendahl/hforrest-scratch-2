a05dbae6638dc2a9ffb8fbc9cfb3a0a530b807f8
Russell Hofvendahl <rhofvendahl@gmail.com>
2023-03-07 14:27:05 -0800

    add back sculpture & half of images

--- src/constants/imageList.js
+++ src/constants/imageList.js
@@ -2,57 +2,57 @@
 // import coyoteskull1 from '../img/coyoteskull1.jpeg';
 // import coyoteskull2 from '../img/coyoteskull2.jpeg';
 // import coyoteskull3 from '../img/coyoteskull3.jpeg';
-import twoweekspay from '../img/2weekspay.jpeg';
+// import twoweekspay from '../img/2weekspay.jpeg';
 import bronze01 from '../img/bronze01.jpg';
 import bronze02 from '../img/bronze02.jpg';
 import bronze03 from '../img/bronze03.jpg';
 import bronze05 from '../img/bronze05.jpg';
 import bronze06 from '../img/bronze06.jpg';
-import bronze06extra1 from '../img/bronze06extra1.jpg';
+// import bronze06extra1 from '../img/bronze06extra1.jpg';
 import bronze07 from '../img/bronze07.jpg';
 import bronze08 from '../img/bronze08.jpg';
 import bronze09 from '../img/bronze09.jpg';
 // import bronze10 from '../img/bronze10.jpg';
-import bronze11 from '../img/bronze11.jpg';
-import bronze12 from '../img/bronze12.jpg';
-import bronze13 from '../img/bronze13.jpg';
-import bronze14 from '../img/bronze14.jpg';
-import bronze15 from '../img/bronze15.jpg';
-import bronze16 from '../img/bronze16.jpg';
-import bronze23extra1 from '../img/bronze23extra1.jpg';
-import bronze18 from '../img/bronze18.jpeg';
-import bronze16extra1 from '../img/bronze16extra1.jpeg';
-// import bronze20 from '../img/bronze20.jpeg';
-import bronze21 from '../img/bronze21.jpeg';
-import tillcoyoteextra1 from '../img/tillcoyoteextra1.jpeg';
-import bronze23 from '../img/bronze23.jpeg';
-import bronze18extra1 from '../img/bronze18extra1.jpeg';
-import tillcoyoteextra2 from '../img/tillcoyoteextra2.jpeg';
-import bronze23extra2 from '../img/bronze23extra2.jpg';
-import bronzeelk from '../img/bronzeelk.jpg';
-// import bronzehardypatches from '../img/bronzehardypatches.jpeg';
-// import bronzemultiple01 from '../img/bronzemultiple01.jpg';
-import bronzework01 from '../img/bronzework01.jpg';
-import bronzework02 from '../img/bronzework02.jpg';
-import tillcoyote02 from '../img/tillcoyote02.jpg';
-import tillcoyote from '../img/tillcoyote.jpg';
-import tillcoyote03 from '../img/tillcoyote03.jpg';
-// import elkspear from '../img/elkspear.jpg';
-import bronze24 from '../img/bronze24.jpg';
-import bronze25 from '../img/bronze25.jpeg';
-import bronze31 from '../img/bronze31.jpg';
-import bronze32 from '../img/bronze32.jpg';
-import bronze33 from '../img/bronze33.jpg';
-import bronze34 from '../img/bronze34.jpg';
-import bronze35 from '../img/bronze35.jpg';
-import bronze36 from '../img/bronze36.jpg';
-import bronze37 from '../img/bronze37.jpg';
-import bronze38 from '../img/bronze38.jpg';
-import bronze39 from '../img/bronze39.jpg';
-import bronze40 from '../img/bronze40.jpg';
-import bronze41 from '../img/bronze41.jpg';
-import bronze42 from '../img/bronze42.jpg';
-import bronze43 from '../img/bronze43.jpg';
+// import bronze11 from '../img/bronze11.jpg';
+// import bronze12 from '../img/bronze12.jpg';
+// import bronze13 from '../img/bronze13.jpg';
+// import bronze14 from '../img/bronze14.jpg';
+// import bronze15 from '../img/bronze15.jpg';
+// import bronze16 from '../img/bronze16.jpg';
+// import bronze23extra1 from '../img/bronze23extra1.jpg';
+// import bronze18 from '../img/bronze18.jpeg';
+// import bronze16extra1 from '../img/bronze16extra1.jpeg';
+// // import bronze20 from '../img/bronze20.jpeg';
+// import bronze21 from '../img/bronze21.jpeg';
+// import tillcoyoteextra1 from '../img/tillcoyoteextra1.jpeg';
+// import bronze23 from '../img/bronze23.jpeg';
+// import bronze18extra1 from '../img/bronze18extra1.jpeg';
+// import tillcoyoteextra2 from '../img/tillcoyoteextra2.jpeg';
+// import bronze23extra2 from '../img/bronze23extra2.jpg';
+// import bronzeelk from '../img/bronzeelk.jpg';
+// // import bronzehardypatches from '../img/bronzehardypatches.jpeg';
+// // import bronzemultiple01 from '../img/bronzemultiple01.jpg';
+// import bronzework01 from '../img/bronzework01.jpg';
+// import bronzework02 from '../img/bronzework02.jpg';
+// import tillcoyote02 from '../img/tillcoyote02.jpg';
+// import tillcoyote from '../img/tillcoyote.jpg';
+// import tillcoyote03 from '../img/tillcoyote03.jpg';
+// // import elkspear from '../img/elkspear.jpg';
+// import bronze24 from '../img/bronze24.jpg';
+// import bronze25 from '../img/bronze25.jpeg';
+// import bronze31 from '../img/bronze31.jpg';
+// import bronze32 from '../img/bronze32.jpg';
+// import bronze33 from '../img/bronze33.jpg';
+// import bronze34 from '../img/bronze34.jpg';
+// import bronze35 from '../img/bronze35.jpg';
+// import bronze36 from '../img/bronze36.jpg';
+// import bronze37 from '../img/bronze37.jpg';
+// import bronze38 from '../img/bronze38.jpg';
+// import bronze39 from '../img/bronze39.jpg';
+// import bronze40 from '../img/bronze40.jpg';
+// import bronze41 from '../img/bronze41.jpg';
+// import bronze42 from '../img/bronze42.jpg';
+// import bronze43 from '../img/bronze43.jpg';
 
 
 export const imageList = [
@@ -65,81 +65,81 @@ export const imageList = [
     depth: "6",
     description: null
   },
-  {
-    src: twoweekspay,
-    title: "Two Weeks Pay",
-    alt: "silver colored baby skull covered in barbed wire",
-    length: "10",
-    width: "6",
-    depth: "10",
-    description: null,
-  },
-  {
-    src: bronze16,
-    title: "Ode to a Ditchrider",
-    alt: "bronze artwork with a coyote skull",
-    length: "30",
-    width: "24",
-    depth: "4",
-    description: null,
-    moreImages: [
-      {
-        src: bronze16extra1,
-        title: "Ode to a Ditchrider",
-        alt: "coyote skull art on a fence",
-        description: null,
-      },
-    ]
-  },
-  {
-    src: bronze23,
-    title: "An Alpine Idyll, an Homage",
-    alt: "An Alpine Idyll, an Homage",
-    length: "72",
-    width: "8",
-    depth: "4",
-    description: null,
-    moreImages: [
-      {
-        src: bronze23extra1,
-        title: "An Alpine Idyll, an Homage",
-        alt: "An Alpine Idyll, an Homage second picture",
-        description: null,
-      },
-      {
-        src: bronze23extra2,
-        title: "An Alpine Idyll, an Homage",
-        alt: "An Alpine Idyll, an Homage third picture",
-        description: null,
-      },  
-    ]
-  },
-  {
-    src: bronzework01,
-    title: "One is None",
-    alt: "One is None",
-    length: "16",
-    width: "9",
-    depth: "6",
-    description: null,
-    moreImages: [
-      {
-        src: bronzework02,
-        title: "One is None",
-        alt: "One is None",
-        description: null,
-      },
-    ]
-  },  
-  {
-    src: bronzeelk,
-    title: "One is None",
-    alt: "One is None",
-    length: "16",
-    width: "9",
-    depth: "6",
-    description: null,
-  },
+  // {
+  //   src: twoweekspay,
+  //   title: "Two Weeks Pay",
+  //   alt: "silver colored baby skull covered in barbed wire",
+  //   length: "10",
+  //   width: "6",
+  //   depth: "10",
+  //   description: null,
+  // },
+  // {
+  //   src: bronze16,
+  //   title: "Ode to a Ditchrider",
+  //   alt: "bronze artwork with a coyote skull",
+  //   length: "30",
+  //   width: "24",
+  //   depth: "4",
+  //   description: null,
+  //   moreImages: [
+  //     {
+  //       src: bronze16extra1,
+  //       title: "Ode to a Ditchrider",
+  //       alt: "coyote skull art on a fence",
+  //       description: null,
+  //     },
+  //   ]
+  // },
+  // {
+  //   src: bronze23,
+  //   title: "An Alpine Idyll, an Homage",
+  //   alt: "An Alpine Idyll, an Homage",
+  //   length: "72",
+  //   width: "8",
+  //   depth: "4",
+  //   description: null,
+  //   moreImages: [
+  //     {
+  //       src: bronze23extra1,
+  //       title: "An Alpine Idyll, an Homage",
+  //       alt: "An Alpine Idyll, an Homage second picture",
+  //       description: null,
+  //     },
+  //     {
+  //       src: bronze23extra2,
+  //       title: "An Alpine Idyll, an Homage",
+  //       alt: "An Alpine Idyll, an Homage third picture",
+  //       description: null,
+  //     },  
+  //   ]
+  // },
+  // {
+  //   src: bronzework01,
+  //   title: "One is None",
+  //   alt: "One is None",
+  //   length: "16",
+  //   width: "9",
+  //   depth: "6",
+  //   description: null,
+  //   moreImages: [
+  //     {
+  //       src: bronzework02,
+  //       title: "One is None",
+  //       alt: "One is None",
+  //       description: null,
+  //     },
+  //   ]
+  // },  
+  // {
+  //   src: bronzeelk,
+  //   title: "One is None",
+  //   alt: "One is None",
+  //   length: "16",
+  //   width: "9",
+  //   depth: "6",
+  //   description: null,
+  // },
   {
     src: bronze02,
     title: "Ride",
@@ -167,77 +167,77 @@ export const imageList = [
     depth: "5",
     description: null,
   },
-  {
-    src: tillcoyote02,
-    title: "Let er Buck",
-    alt: "Let er Buck",
-    length: "18",
-    width: "18",
-    depth: "5",
-    description: null,
-    moreImages: [
-      {
-        src: tillcoyote,
-        title: "Let er Buck",
-        alt: "Let er Buck second picture",
-        description: null,
-      },
-      {
-        src: tillcoyote03,
-        title: "Let er Buck",
-        alt: "Let er Buck third picture",
-        description: null,
-      },
-      {
-        src: tillcoyoteextra1,
-        title: "Let er Buck",
-        alt: "Let er Buck fourth picture",
-        description: null,
-      },
-      {
-        src: tillcoyoteextra2,
-        title: "Let er Buck",
-        alt: "Let er Buck fifth picture",
-        description: null,
-      },
-    ]
-  },
-  {
-    src: bronze11,
-    title: "Coyote #1",
-    alt: "Coyote #1",
-    length: "23",
-    width: "8",
-    depth: "5",
-    description: null,
-  },
-  {
-    src: bronze12,
-    title: "Coyote #2",
-    alt: "Coyote #2",
-    length: "8",
-    width: "4",
-    depth: "3",
-    description: null,
-  },
-  {
-    src: bronze13,
-    title: "Coyote #3",
-    alt: "Coyote #3",
-    length: "8",
-    width: "4",
-    depth: "3",
-    description: null,
-  },
-  {
-    src: bronze21,
-    title: "Coyote #4",
-    alt: "Coyote #4",
-    length: "10",
-    width: "9",
-    depth: "4",
-    description: null,
-  },
+  // {
+  //   src: tillcoyote02,
+  //   title: "Let er Buck",
+  //   alt: "Let er Buck",
+  //   length: "18",
+  //   width: "18",
+  //   depth: "5",
+  //   description: null,
+  //   moreImages: [
+  //     {
+  //       src: tillcoyote,
+  //       title: "Let er Buck",
+  //       alt: "Let er Buck second picture",
+  //       description: null,
+  //     },
+  //     {
+  //       src: tillcoyote03,
+  //       title: "Let er Buck",
+  //       alt: "Let er Buck third picture",
+  //       description: null,
+  //     },
+  //     {
+  //       src: tillcoyoteextra1,
+  //       title: "Let er Buck",
+  //       alt: "Let er Buck fourth picture",
+  //       description: null,
+  //     },
+  //     {
+  //       src: tillcoyoteextra2,
+  //       title: "Let er Buck",
+  //       alt: "Let er Buck fifth picture",
+  //       description: null,
+  //     },
+  //   ]
+  // },
+  // {
+  //   src: bronze11,
+  //   title: "Coyote #1",
+  //   alt: "Coyote #1",
+  //   length: "23",
+  //   width: "8",
+  //   depth: "5",
+  //   description: null,
+  // },
+  // {
+  //   src: bronze12,
+  //   title: "Coyote #2",
+  //   alt: "Coyote #2",
+  //   length: "8",
+  //   width: "4",
+  //   depth: "3",
+  //   description: null,
+  // },
+  // {
+  //   src: bronze13,
+  //   title: "Coyote #3",
+  //   alt: "Coyote #3",
+  //   length: "8",
+  //   width: "4",
+  //   depth: "3",
+  //   description: null,
+  // },
+  // {
+  //   src: bronze21,
+  //   title: "Coyote #4",
+  //   alt: "Coyote #4",
+  //   length: "10",
+  //   width: "9",
+  //   depth: "4",
+  //   description: null,
+  // },
   {
     src: bronze06,
     title: "Anodyne",
@@ -246,14 +246,14 @@ export const imageList = [
     width: "5",
     depth: "7",
     description: null,
-    moreImages: [
-      {
-        src: bronze06extra1,
-        title: "Anodyne",
-        alt: "Anodyne second picture",
-        description: null,
-      },
-    ]
+    // moreImages: [
+    //   {
+    //     src: bronze06extra1,
+    //     title: "Anodyne",
+    //     alt: "Anodyne second picture",
+    //     description: null,
+    //   },
+    // ]
   },
   {
     src: bronze07,
@@ -273,15 +273,15 @@ export const imageList = [
     depth: "6",
     description: null,
   },
-  {
-    src: bronze14,
-    title: "Coyote #5",
-    alt: "Coyote #5",
-    length: "8",
-    width: "4",
-    depth: "3",
-    description: null,
-  },
+  // {
+  //   src: bronze14,
+  //   title: "Coyote #5",
+  //   alt: "Coyote #5",
+  //   length: "8",
+  //   width: "4",
+  //   depth: "3",
+  //   description: null,
+  // },
   {
     src: bronze09,
     title: "Cow Elk #2",
@@ -291,165 +291,165 @@ export const imageList = [
     depth: "6",
     description: null,
   },
-  {
-    src: bronze18,
-    title: "Sober",
-    alt: "Sober",
-    length: "60",
-    width: "3",
-    depth: "3",
-    description: null,
-    moreImages: [
-      {
-        src: bronze18extra1,
-        title: "Sober",
-        alt: "Sober second picture",
-        description: null,
-      },
-    ]
-  },
-  {
-    src: bronze15,
-    title: "Untitled",
-    alt: "Untitled",
-    length: "8",
-    width: "8",
-    depth: "6",
-    description: null,
-  },
-  {
-    src: bronze24,
-    title: "bronze24",
-    alt: "bronze24",
-    length: null,
-    width: null,
-    depth: null,
-    description: null,
-  },
-  {
-    src: bronze25,
-    title: "bronze25",
-    alt: "bronze25",
-    length: null,
-    width: null,
-    depth: null,
-    description: null,
-  },
-  {
-    src: bronze31,
-    title: "bronze31",
-    alt: "bronze31",
-    length: null,
-    width: null,
-    depth: null,
-    description: null,
-  },
-  {
-    src: bronze32,
-    title: "bronze32",
-    alt: "bronze32",
-    length: null,
-    width: null,
-    depth: null,
-    description: null,
-  },
-  {
-    src: bronze33,
-    title: "bronze33",
-    alt: "bronze33",
-    length: null,
-    width: null,
-    depth: null,
-    description: null,
-  },
-  {
-    src: bronze34,
-    title: "bronze34",
-    alt: "bronze34",
-    length: null,
-    width: null,
-    depth: null,
-    description: null,
-  },
-  {
-    src: bronze35,
-    title: "bronze35",
-    alt: "bronze35",
-    length: null,
-    width: null,
-    depth: null,
-    description: null,
-  },
-  {
-    src: bronze36,
-    title: "bronze36",
-    alt: "bronze36",
-    length: null,
-    width: null,
-    depth: null,
-    description: null,
-  },
-  {
-    src: bronze37,
-    title: "bronze37",
-    alt: "bronze37",
-    length: null,
-    width: null,
-    depth: null,
-    description: null,
-  },
-  {
-    src: bronze38,
-    title: "bronze38",
-    alt: "bronze38",
-    length: null,
-    width: null,
-    depth: null,
-    description: null,
-  },
-  {
-    src: bronze39,
-    title: "bronze39",
-    alt: "bronze39",
-    length: null,
-    width: null,
-    depth: null,
-    description: null,
-  },
-  {
-    src: bronze40,
-    title: "bronze40",
-    alt: "bronze40",
-    length: null,
-    width: null,
-    depth: null,
-    description: null,
-  },
-  {
-    src: bronze41,
-    title: "bronze41",
-    alt: "bronze41",
-    length: null,
-    width: null,
-    depth: null,
-    description: null,
-  },
-  {
-    src: bronze42,
-    title: "bronze42",
-    alt: "bronze42",
-    length: null,
-    width: null,
-    depth: null,
-    description: null,
-  },
-  {
-    src: bronze43,
-    title: "bronze43",
-    alt: "bronze43",
-    length: null,
-    width: null,
-    depth: null,
-    description: null,
-  },
+  // {
+  //   src: bronze18,
+  //   title: "Sober",
+  //   alt: "Sober",
+  //   length: "60",
+  //   width: "3",
+  //   depth: "3",
+  //   description: null,
+  //   moreImages: [
+  //     {
+  //       src: bronze18extra1,
+  //       title: "Sober",
+  //       alt: "Sober second picture",
+  //       description: null,
+  //     },
+  //   ]
+  // },
+  // {
+  //   src: bronze15,
+  //   title: "Untitled",
+  //   alt: "Untitled",
+  //   length: "8",
+  //   width: "8",
+  //   depth: "6",
+  //   description: null,
+  // },
+  // {
+  //   src: bronze24,
+  //   title: "bronze24",
+  //   alt: "bronze24",
+  //   length: null,
+  //   width: null,
+  //   depth: null,
+  //   description: null,
+  // },
+  // {
+  //   src: bronze25,
+  //   title: "bronze25",
+  //   alt: "bronze25",
+  //   length: null,
+  //   width: null,
+  //   depth: null,
+  //   description: null,
+  // },
+  // {
+  //   src: bronze31,
+  //   title: "bronze31",
+  //   alt: "bronze31",
+  //   length: null,
+  //   width: null,
+  //   depth: null,
+  //   description: null,
+  // },
+  // {
+  //   src: bronze32,
+  //   title: "bronze32",
+  //   alt: "bronze32",
+  //   length: null,
+  //   width: null,
+  //   depth: null,
+  //   description: null,
+  // },
+  // {
+  //   src: bronze33,
+  //   title: "bronze33",
+  //   alt: "bronze33",
+  //   length: null,
+  //   width: null,
+  //   depth: null,
+  //   description: null,
+  // },
+  // {
+  //   src: bronze34,
+  //   title: "bronze34",
+  //   alt: "bronze34",
+  //   length: null,
+  //   width: null,
+  //   depth: null,
+  //   description: null,
+  // },
+  // {
+  //   src: bronze35,
+  //   title: "bronze35",
+  //   alt: "bronze35",
+  //   length: null,
+  //   width: null,
+  //   depth: null,
+  //   description: null,
+  // },
+  // {
+  //   src: bronze36,
+  //   title: "bronze36",
+  //   alt: "bronze36",
+  //   length: null,
+  //   width: null,
+  //   depth: null,
+  //   description: null,
+  // },
+  // {
+  //   src: bronze37,
+  //   title: "bronze37",
+  //   alt: "bronze37",
+  //   length: null,
+  //   width: null,
+  //   depth: null,
+  //   description: null,
+  // },
+  // {
+  //   src: bronze38,
+  //   title: "bronze38",
+  //   alt: "bronze38",
+  //   length: null,
+  //   width: null,
+  //   depth: null,
+  //   description: null,
+  // },
+  // {
+  //   src: bronze39,
+  //   title: "bronze39",
+  //   alt: "bronze39",
+  //   length: null,
+  //   width: null,
+  //   depth: null,
+  //   description: null,
+  // },
+  // {
+  //   src: bronze40,
+  //   title: "bronze40",
+  //   alt: "bronze40",
+  //   length: null,
+  //   width: null,
+  //   depth: null,
+  //   description: null,
+  // },
+  // {
+  //   src: bronze41,
+  //   title: "bronze41",
+  //   alt: "bronze41",
+  //   length: null,
+  //   width: null,
+  //   depth: null,
+  //   description: null,
+  // },
+  // {
+  //   src: bronze42,
+  //   title: "bronze42",
+  //   alt: "bronze42",
+  //   length: null,
+  //   width: null,
+  //   depth: null,
+  //   description: null,
+  // },
+  // {
+  //   src: bronze43,
+  //   title: "bronze43",
+  //   alt: "bronze43",
+  //   length: null,
+  //   width: null,
+  //   depth: null,
+  //   description: null,
+  // },
 ]
